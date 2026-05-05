f8d45a76098cdbadac98f6430e0a3b8ba9cb8247
Rex <rexrajan2001@gmail.com>
2026-05-05 13:29:21 +0530

    ArmstrongNum

--- For loop/src/Armstrong_No_Check.java
+++ For loop/src/Armstrong_No_Check.java
@@ -1,6 +1,34 @@
 import java.util.*;public class Armstrong_No_Check {
     public static void main(String[] args) {
-        long actual_number = 370;
+
+        long actual_number = 9474;
+        long number = actual_number;
+        long sum = 0;
+
+        // Step 1: Count digits
+        int digits = String.valueOf(actual_number).length();
+
+        // Step 2: Calculate Armstrong sum
+        while (number > 0) {
+            long mod = number % 10;
+
+            long power = 1;
+            for (int i = 0; i < digits; i++) {
+                power = power * mod;
+            }
+
+            sum = sum + power;
+            number = number / 10;
+        }
+
+        // Step 3: Check
+        if (actual_number == sum)
+            System.out.println("Hence, It's an Armstrong number");
+        else
+            System.out.println("It's not an Armstrong number");
+
+
+        /*long actual_number = 370;
         long number = actual_number;
         long sum = 0;
         for (int i = 1; number > 0; i = i + 1){
@@ -13,5 +41,5 @@ import java.util.*;public class Armstrong_No_Check {
             System.out.println("Hence, It's an armstrong number");
         else
             System.out.println("It's not an armstrong number");
-    }
+    }*/
 }
