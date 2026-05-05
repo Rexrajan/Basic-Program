7cc8846a3b96a63fe354401cda2294b58ad341dd
Rex <rexrajan2001@gmail.com>
2026-05-05 13:45:00 +0530

    changed

--- For loop/src/Armstrong_No_Check.java
+++ For loop/src/Armstrong_No_Check.java
@@ -1,32 +1,31 @@
 import java.util.*;public class Armstrong_No_Check {
     public static void main(String[] args) {
 
-        long actual_number = 9474;
-        long number = actual_number;
-        long sum = 0;
+                long actual_number = 153;
+                long number = actual_number;
+                long sum = 0;
 
-        // Step 1: Count digits
-        int digits = String.valueOf(actual_number).length();
+                // Step 1: Count digits
+                int digits = String.valueOf(actual_number).length();
 
-        // Step 2: Calculate Armstrong sum
-        while (number > 0) {
-            long mod = number % 10;
+                // Step 2: Calculate Armstrong sum
+                while (number > 0) {
+                    long mod = number % 10;
 
-            long power = 1;
-            for (int i = 0; i < digits; i++) {
-                power = power * mod;
-            }
+                    long power = 1;
+                    for (int i = 0; i < digits; i++) {
+                        power = power * mod;
+                    }
 
-            sum = sum + power;
-            number = number / 10;
-        }
+                    sum = sum + power;
+                    number = number / 10;
+                }
 
-        // Step 3: Check
-        if (actual_number == sum)
-            System.out.println("Hence, It's an Armstrong number");
-        else
-            System.out.println("It's not an Armstrong number");
-
+                // Step 3: Check
+                if (actual_number == sum)
+                    System.out.println("Hence, It's an Armstrong number");
+                else
+                    System.out.println("It's not an Armstrong number");
 
         /*long actual_number = 370;
         long number = actual_number;
@@ -40,6 +39,6 @@ import java.util.*;public class Armstrong_No_Check {
         if (actual_number == sum)
             System.out.println("Hence, It's an armstrong number");
         else
-            System.out.println("It's not an armstrong number");
-    }*/
+            System.out.println("It's not an armstrong number");*/
+    }
 }
